91ba1c95e0b8428911588d8f4f7eae405039cea7
DESKTOP-FQTBGS1\Randomality <brainlicker1@yahoo.com>
2022-05-06 20:17:00 -0400

    Moves smoothly between two points cause Lerp is apprently a thing?

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [SerializeField] List<TileWaypoints> waypointPath = new List<TileWaypoints>();
-    [SerializeField] float enemyMoveSpeed = 1f;
+    [SerializeField][Range(0f,5f)] float enemyMoveSpeed = 1f;
+    //[SerializeField] float  travelPercentModifier = 0f;
 
     void Start()
     {
@@ -23,8 +24,18 @@ public class EnemyMovement : MonoBehaviour
     {   
         foreach (TileWaypoints tilewaypoint in waypointPath)
         {   
-            transform.position = tilewaypoint.transform.position;
-            yield return new WaitForSeconds(enemyMoveSpeed);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = tilewaypoint.transform.position;
+            float travelPercent = 0f;
+            //transform.position = tilewaypoint.transform.position;
+           // yield return new WaitForSeconds(enemyMoveSpeed);
+           
+            transform.LookAt(endPosition);
+           while(travelPercent < 1f){
+               travelPercent += Time.deltaTime * enemyMoveSpeed;
+               transform.position = Vector3.Lerp(startPosition,endPosition,travelPercent);
+               yield return new WaitForEndOfFrame();
+           }
         }
     }
 }
